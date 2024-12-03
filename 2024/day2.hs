879454810af3516494c7d4e3a0a92ff5242ccc98
Jan <jangalliker2003@gmail.com>
2024-12-03 10:25:33 +0100

    optimize day 2

--- 2024/day2.hs
+++ 2024/day2.hs
@@ -2,7 +2,7 @@ solveDay2Part1 :: FilePath -> IO Int
 solveDay2Part1 = solve part1
 
 solveDay2Part2 :: FilePath -> IO Int
-solveDay2Part2 = solve part2 
+solveDay2Part2 = solve part2
 
 --Common Functions
 solve :: (String -> a) -> FilePath -> IO a
@@ -13,12 +13,6 @@ solve solver fileName = do
 prepareInput :: String -> [[Int]]
 prepareInput = map (map read  . words) . lines
 
-isDecreasing :: [Int] -> Bool
-isDecreasing [x] = True
-isDecreasing (x1:x2:xs)
- | x1-1 >= x2 && x1-3 <= x2 = isDecreasing (x2:xs)
- | otherwise = False
-
 isIncreasing :: [Int] -> Bool
 isIncreasing [x] = True
 isIncreasing (x1:x2:xs)
@@ -27,21 +21,18 @@ isIncreasing (x1:x2:xs)
 
 --Part 1
 part1 :: String -> Int
-part1 = length . filter (\list -> isDecreasing list || isIncreasing list) . prepareInput
+part1 = length . filter (\list -> isIncreasing (reverse list) || isIncreasing list) . prepareInput
 
 --Part 2
 part2 :: String -> Int
 part2 = length . filter listMatchesCirteria . prepareInput
 
 listMatchesCirteria :: [Int] -> Bool
-listMatchesCirteria (x:xs) =
-    isDecreasingWithInaccuracy (x:xs) || isIncreasingWithInaccuracy (x:xs) || isDecreasing xs || isIncreasing xs
-
-isDecreasingWithInaccuracy :: [Int] -> Bool
-isDecreasingWithInaccuracy [x] = True
-isDecreasingWithInaccuracy (x1:x2:xs)
- | x1-1 >= x2 && x1-3 <= x2 = isDecreasingWithInaccuracy (x2:xs)
- | otherwise = isDecreasing (x1:xs)
+listMatchesCirteria list =
+    isIncreasingWithInaccuracy (reverse list) ||
+    isIncreasingWithInaccuracy list ||
+    (isIncreasing . tail . reverse) list ||
+    (isIncreasing . tail) list
 
 isIncreasingWithInaccuracy :: [Int] -> Bool
 isIncreasingWithInaccuracy [x] = True
