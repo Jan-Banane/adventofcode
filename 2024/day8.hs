de31a070cc64b6c5bbe4dade1d5200b41a0bd246
Jan <jangalliker2003@gmail.com>
2025-07-05 14:50:49 +0200

    refactor day 8

--- 2024/day8.hs
+++ 2024/day8.hs
@@ -2,46 +2,60 @@
 {-# HLINT ignore "Use tuple-section" #-}
 import qualified Data.Set as S
 
--- Common Funcs
+solveDay8Part1 :: FilePath -> IO Int
+solveDay8Part1 = solve part1
+
+solveDay8Part2 :: FilePath -> IO Int
+solveDay8Part2 = solve part2
+
+--Common Funcs
 solve :: (String -> a) -> FilePath -> IO a
 solve solver fileName = do
     text <- readFile fileName
     return $ solver text
 
-part1 input = S.size $ filterAntinodes (length towerMap, length (head towerMap)) $ S.unions $ map (S.unions . S.toList . S.map (uncurry getAntinodes) . getAllCombinations . getLocations towerMap) allChars
-    where towerMap = lines input
-
-part2 input = S.size $ S.fromList $ filterAntinodes' (length towerMap, length (head towerMap)) $ concatMap ( concatMap (uncurry getAntinodes') . S.toList . getAllCombinations . getLocations towerMap) allChars
-    where towerMap = lines input
-
-allChars :: [Char]
-allChars = ['a'..'z'] ++ ['A'..'Z'] ++ ['0'..'9']
+allRelevantChars :: [Char]
+allRelevantChars = ['a'..'z'] ++ ['A'..'Z'] ++ ['0'..'9']
 
 getAllCombinations :: S.Set (Int, Int) -> S.Set ((Int,Int),(Int,Int))
-getAllCombinations locations = S.fromList $ permutate $ S.toList locations
+getAllCombinations = S.fromList . permutations . S.toList
 
-permutate :: [b] -> [(b, b)]
-permutate (x:xs) = map (\a -> (x,a)) xs ++ permutate xs
-permutate [] = []
+permutations :: [b] -> [(b, b)]
+permutations (x:xs) = map (\a -> (x,a)) xs ++ permutations xs
+permutations [] = []
 
-getLocations :: [String] -> Char -> S.Set (Int,Int)
-getLocations towerMap char = S.fromList $ concatMap (\(a,list) -> map (\b -> (a,b)) list) (filter (\(_,b) -> b /= []) $ zip [0..] $ map (getLocationsLine char) towerMap)
+coordinatesOfChar :: [String] -> Char -> S.Set (Int,Int)
+coordinatesOfChar towerMap char = S.fromList $ concatMap (\(a,list) -> map (\b -> (a,b)) list) (filter (\(_,b) -> b /= []) $ zip [0..] $ map (indexOfChar char) towerMap)
 
-getLocationsLine :: Char -> String -> [Int]
-getLocationsLine char = map fst . filter (\(_,b) -> b == char) . zip [0..]
+indexOfChar :: Char -> String -> [Int]
+indexOfChar char = map fst . filter (\(_,b) -> b == char) . zip [0..]
 
-getAntinodes :: (Int,Int) -> (Int, Int) -> S.Set (Int,Int)
-getAntinodes (x1,y1) (x2,y2) = S.fromList [(x2+distanceX,y2+distanceY),(x1-distanceX,y1-distanceY)]
-    where distanceX = x2-x1
-          distanceY = y2-y1
+filterAntinodes :: Int -> Int -> S.Set (Int,Int) -> S.Set (Int,Int)
+filterAntinodes xRange yRange = S.filter (\(x,y) -> x >= 0 && x < xRange && y >= 0 && y < yRange)
 
-getAntinodes' :: (Int,Int) -> (Int, Int) -> [(Int,Int)]
-getAntinodes' (x1,y1) (x2,y2) = concatMap ((\(distX,distY) -> [(x2+distX,y2+distY),(x1-distX,y1-distY)]) . (\num -> (distanceX * num,distanceY * num))) [0..50]
-    where distanceX = x2-x1
-          distanceY = y2-y1
+--Part 1
+part1 :: String -> Int
+part1 input = S.size $ antinodeFilter $ S.unions $ map (S.unions . S.map getAdjacentAntinodes . getAllCombinations . coordinatesOfChar towerMap) allRelevantChars
+ where 
+    towerMap = lines input
+    antinodeFilter = filterAntinodes (length towerMap) (length $ head towerMap)
 
-filterAntinodes' :: (Int,Int) -> [(Int,Int)] -> [(Int,Int)]
-filterAntinodes' (xRange,yRange) = filter (\(x,y) -> x >= 0 && x < xRange && y >= 0 && y < yRange)
+getAdjacentAntinodes :: ((Int,Int),(Int, Int)) -> S.Set (Int,Int)
+getAdjacentAntinodes ((x1,y1),(x2,y2)) = S.fromList [(x2+distanceX,y2+distanceY),(x1-distanceX,y1-distanceY)]
+ where
+    distanceX = x2-x1
+    distanceY = y2-y1
 
-filterAntinodes :: (Int,Int) -> S.Set (Int,Int) -> S.Set (Int,Int)
-filterAntinodes (xRange,yRange) = S.filter (\(x,y) -> x >= 0 && x < xRange && y >= 0 && y < yRange)
+--Part 2
+part2 :: String -> Int
+part2 input = S.size $ antinodeFilter $ S.unions $ map (S.unions . S.map allAntinodesFinder . getAllCombinations . coordinatesOfChar towerMap) allRelevantChars
+ where
+    towerMap = lines input
+    antinodeFilter = filterAntinodes (length towerMap) (length $ head towerMap)
+    allAntinodesFinder = getAllAntinodes (length towerMap) (length (head towerMap))
+
+getAllAntinodes :: Int -> Int -> ((Int,Int),(Int, Int)) -> S.Set (Int,Int)
+getAllAntinodes xRange yRange ((x1,y1),(x2,y2)) = S.fromList $ concatMap ((\(distX,distY) -> [(x2+distX,y2+distY),(x1-distX,y1-distY)]) . (\num -> (distanceX * num,distanceY * num))) [0..(max (div xRange distanceX) (div yRange distanceY))]
+ where
+    distanceX = x2-x1
+    distanceY = y2-y1
